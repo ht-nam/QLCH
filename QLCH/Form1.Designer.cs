4273a9c94c3c356ea24f648f7e14a465051989ec
ht-nam <hoangthanhnam10@gmail.com>
2022-02-28 21:11:33 +0700

    Add project files.

---
+++ QLCH/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace QLCH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btLogin = new System.Windows.Forms.Button();
+            this.lbPW = new System.Windows.Forms.Label();
+            this.lbID = new System.Windows.Forms.Label();
+            this.tbPW = new System.Windows.Forms.TextBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btLogin);
+            this.panel1.Controls.Add(this.lbPW);
+            this.panel1.Controls.Add(this.lbID);
+            this.panel1.Controls.Add(this.tbPW);
+            this.panel1.Controls.Add(this.tbID);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLogin.Location = new System.Drawing.Point(154, 270);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(143, 32);
+            this.btLogin.TabIndex = 4;
+            this.btLogin.Text = "Đăng nhập";
+            this.btLogin.UseVisualStyleBackColor = false;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
+            // 
+            // lbPW
+            // 
+            this.lbPW.AutoSize = true;
+            this.lbPW.BackColor = System.Drawing.Color.Transparent;
+            this.lbPW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPW.Location = new System.Drawing.Point(35, 176);
+            this.lbPW.Name = "lbPW";
+            this.lbPW.Size = new System.Drawing.Size(93, 25);
+            this.lbPW.TabIndex = 3;
+            this.lbPW.Text = "Mật khẩu";
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.BackColor = System.Drawing.Color.Transparent;
+            this.lbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbID.Location = new System.Drawing.Point(35, 89);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(126, 25);
+            this.lbID.TabIndex = 2;
+            this.lbID.Text = "Mã cửa hàng";
+            // 
+            // tbPW
+            // 
+            this.tbPW.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPW.Location = new System.Drawing.Point(50, 204);
+            this.tbPW.Name = "tbPW";
+            this.tbPW.PasswordChar = '*';
+            this.tbPW.Size = new System.Drawing.Size(218, 34);
+            this.tbPW.TabIndex = 1;
+            this.tbPW.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPW_KeyDown);
+            // 
+            // tbID
+            // 
+            this.tbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbID.Location = new System.Drawing.Point(50, 117);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(218, 34);
+            this.tbID.TabIndex = 0;
+            this.tbID.Leave += new System.EventHandler(this.tbID_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 450);
+            this.panel2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(514, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbPW;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label lbPW;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Button btLogin;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ QLCH/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+namespace QLCH
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseConfig fbCf = new FirebaseConfig()
+        {
+            AuthSecret = "QhNihnCtn3mTIG47KS1ET0tUpQ8wfW9hMXQnkhLW",
+            BasePath = "https://htnam-72b75-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        string pw = "";
+
+        SqlConnection conn = null;
+        SqlDataAdapter adapter = null;
+        DataTable db = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel2.Hide();
+            string cnt = "Data Source = DESKTOP-KNN7K79; Initial Catalog = QLCH; Integrated Security = True";
+            conn = new SqlConnection(cnt);
+            //string query = "Select * from ";
+            //adapter = new SqlDataAdapter(query, conn);
+            //adapter.Fill(db);
+            //dataGridView1.DataSource = db;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FirebaseClient(fbCf);
+                FirebaseResponse testCon = client.Get("Store");
+                if (testCon.StatusCode.ToString() != "OK")
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Connection error");
+            }
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            if (pw != tbPW.Text)
+            {
+                MessageBox.Show("Wrong id or password");
+            } else
+            {
+                panel1.Hide();
+                panel2.Show();
+            }
+        }
+
+        private void tbID_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                FirebaseResponse response = client.Get("Store/" + tbID.Text);
+                if (response.StatusCode.ToString() != "OK")
+                    throw new Exception();
+                pw = response.ResultAs<string>();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection error");
+            }
+        }
+
+        private void tbPW_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btLogin.PerformClick();
+            }
+        }
+    }
+}
